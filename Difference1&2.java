53073a225a0f8e2307f19fd14ae5d8489c5d55c9
Babatundegold <babatundegold63@gmail.com>
2025-08-19 01:32:15 +0100

    Add files via upload

---
+++ Difference1&2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Difference1&2{
+public static void main(String[] arg){
+Scanner input = new Scanner(System.in);
+System.out.print("enter correct number ");
+int number = input.nextInt();
+	
+int num[] = {4, 5, 3, 6};
+	for(int i = 0; i < num.length; i++){
+		System.out.println(num);
+
+}
+	if(number == num[i]){
+		System.out.print("correct answer");
+
+}
+
+else{
+	System.out.print("Rong answer");
+}
+
+
+}
+
+
+
+}
